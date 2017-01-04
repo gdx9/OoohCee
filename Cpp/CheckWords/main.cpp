9d7cdadbc55f6741d61acfeb504c83749e0511f3
manor9421 <manor9421@yahoo.co.jp>
2017-01-04 20:54:30 +0200

    add more

--- Cpp/CheckWords/main.cpp
+++ Cpp/CheckWords/main.cpp
@@ -11,11 +11,10 @@
 #include <vector>
 #include <algorithm>
 
-int main()
+void readWords(std::vector<std::string> *words)
 {
-
 	std::ifstream wordsFile;
-	const char* filePath = "/home/gdx/Documents/debian.txt";// ???
+	const char* filePath = "/home/gdx/Downloads/words/Disdis.txt";// ???
 	wordsFile.open(filePath);
 
 	if( !wordsFile.is_open() )
@@ -23,15 +22,15 @@ int main()
 		std::cout << "couldn't open a file";
 	}
 
-	std::vector<std::string> words;
-
 	std::string word;
 	while (wordsFile >> word)
 	{
 
 		// erase not a letter symbols and transform string to lower case
 		int len = word.length();
-		while(len>=0)
+		--len;
+
+		while(len >= 0)
 		{
 			if( !isalpha(word[len-1]) )
 				word.erase(word.begin()+len-1);
@@ -41,16 +40,54 @@ int main()
 		}
 
 		// check is that word in a vector
-		if( std::find(words.begin(), words.end(), word ) != words.end()) {
+		if( std::find((*words).begin(), (*words).end(), word ) != (*words).end()) {
 			/* vector contains word */
 		} else {
 			/* vector does not contain word */
-			words.push_back(word);
+			(*words).push_back(word);
 		}
 	}
+	// close a file
+	wordsFile.close();
+}
 
-	for(unsigned int i=0; i<words.size(); ++i){
-		std::cout << words[i] << std::endl;
+void writeWords(std::vector<std::string> *words)
+{
+	std::ofstream writingFile;
+
+	// file for writing new words
+	const char *writingPath = "/home/gdx/Downloads/words/new.txt";
+	writingFile.open(writingPath);
+
+	if( !writingFile.is_open() )
+		std::cout << "error" << std::endl;
+
+	int vecSize = (*words).size();
+	for(unsigned int i=0; i<vecSize; ++i){
+		// show in console
+		std::cout << (*words)[i] << std::endl;
+
+		// write word into file
+		writingFile << (*words)[i] << '\n';
 	}
+
+	// close a file
+	writingFile.close();
+}
+
+int main()
+{
+	std::vector<std::string> words;
+
+	readWords(&words);
+
+	// sorting words
+	std::sort(words.begin(), words.end());
+
+	writeWords(&words);
+
+	words.clear();
+
+	return EXIT_SUCCESS;
 
 }
